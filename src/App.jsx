c35d82941a07f5b05b553255fad89d6314ae82e4
Frankie1414 <fmrodriguez146@gmail.com>
2024-05-21 18:40:29 -0400

    added commented line for demonstration

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Sidebar from './components/Sidebar'
-
+//frankie here 
 function App() {
   return(
 <div>
